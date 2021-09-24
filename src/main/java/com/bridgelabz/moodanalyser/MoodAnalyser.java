65e1fbffb19d0e21d320f177e962f53413a3cecb
govind6498 <govindsingh6498@gmail.com>
2021-09-24 17:03:33 +0530

    REFECTOR:parameterized constructor to take mood message

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,6 +1,13 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser{
+	String message;
+	public MoodAnalyser() {
+		
+	}
+	public void MoodAnalyser(String message) {
+		this.message = message;
+	}
 	public String analyzeMood(String message) {
 		if(message.contains("sad")) {
 			return "SAD";
