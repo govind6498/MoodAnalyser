9dfddf4bedba971589208e2d1558bedd10484b06
govind6498 <govindsingh6498@gmail.com>
2021-09-24 17:19:04 +0530

    REFECTOR:To pass message via constrcutor

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -3,13 +3,13 @@ package com.bridgelabz.moodanalyser;
 public class MoodAnalyser{
 	String message;
 	public MoodAnalyser() {
-		
+		this.message ="";
 	}
 	public void MoodAnalyser(String message) {
 		this.message = message;
 	}
 	public String analyzeMood(String message) {
-		if(message.contains("sad")) {
+		if(message.toLowerCase().contains("sad")) {
 			return "SAD";
 		}
 		else {
