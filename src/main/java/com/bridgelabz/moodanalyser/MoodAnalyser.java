9181ce976c9d94a773d6224898a1476dbb548608
govind6498 <govindsingh6498@gmail.com>
2021-09-24 17:52:58 +0530

    Modified:To validate happy mood for null as paramter

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -4,16 +4,18 @@ public class MoodAnalyser{
 	String message;
 	public MoodAnalyser() {
 		this.message ="This is a happy message";
-		this.message ="This is a sad message";
 	}
 	public MoodAnalyser(String message) {
 		this.message = message;
 	}
 	public String analyzeMood() {
-		if(message.toLowerCase().contains("sad")) {
-			return "SAD";
+		try {
+			if(message.toLowerCase().contains("sad")) {
+				return "SAD";
+			}
+			return "HAPPY";
 		}
-		else {
+		catch (NullPointerException e) {
 			return "HAPPY";
 		}
 	}
