84b5b6a927d702217be337236c57ac5f3435eebf
govind6498 <govindsingh6498@gmail.com>
2021-09-24 18:49:37 +0530

    Modified:To validate hapy mood for null as parameter

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -6,21 +6,27 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
-		MoodAnalyser moodanalyser = new MoodAnalyser("This is a Sad message");
-		String mood = moodanalyser.analyzeMood();
+		MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Sad message");
+		String mood = moodAnalyser.analyzeMood();
 		Assert.assertThat(mood, CoreMatchers.is("SAD"));
 	}
 	@Test
 	public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
 
-		MoodAnalyser moodanalyser = new MoodAnalyser("This is a Happy message");
-		String mood = moodanalyser.analyzeMood();
+		MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Happy message");
+		String mood = moodAnalyser.analyzeMood();
 		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
 	}
 	@Test
-	public void givenMessage_WhenNul_ShouldReturnHapy() {
-		MoodAnalyser moodanalyser = new MoodAnalyser(null);
-		String mood = moodanalyser.analyzeMood();
-		Assert.assertEquals("HAPPY",mood);
+	public void givenMessage_WhenEmpty_ShouldThrowAnalysisException() {
+		try {
+
+			MoodAnalyser moodAnalyser = new MoodAnalyser("");
+			moodAnalyser.analyzeMood();
+		}
+		catch(MoodAnalyserException e) {
+
+			Assert.assertEquals(exceptionType.ENTERED_EMPTY,e.type);			
+		}
 	}
 }
