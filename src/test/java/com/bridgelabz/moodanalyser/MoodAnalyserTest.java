849338d260c4eda23e8f17d5c4363216af0dab3a
govind6498 <govindsingh6498@gmail.com>
2021-09-24 16:43:23 +0530

    Added:Analyse and responde happy or sad mood

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,5 +1,22 @@
 package com.bridgelabz.moodanalyser;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MoodAnalyserTest {
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
 
+		MoodAnalyser moodAnalyzer = new MoodAnalyser();
+		String mood = moodAnalyzer.analyzeMood("This is a sad message");
+		Assert.assertThat(mood, CoreMatchers.is("SAD"));
+	}
+
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
+
+		MoodAnalyser moodAnalyzer = new MoodAnalyser();
+		String mood = moodAnalyzer.analyzeMood("This is a happy message");
+		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+	}
 }
