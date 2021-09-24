c65cd8aad8ded8dbfdec55f0fa137de402561520
govind6498 <govindsingh6498@gmail.com>
2021-09-24 16:56:58 +0530

    Added:function that returns mood string

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser{
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mood Analyser");
+	public String analyzeMood(String message) {
+		if(message.contains("sad")) {
+			return "SAD";
+		}
+		else {
+			return "HAPPY";
+		}
 	}
 }
