404298254dcb25c1d482d331986738638b0f2518
govind6498 <govindsingh6498@gmail.com>
2021-09-24 17:54:54 +0530

    Modified:To validate happy mood for null as paramter

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -6,17 +6,21 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
-
-		MoodAnalyser moodAnalyzer = new MoodAnalyser();
-		String mood = moodAnalyzer.analyzeMood("This is a sad message");
+		MoodAnalyser moodanalyser = new MoodAnalyser("This is a Sad message");
+		String mood = moodanalyser.analyzeMood();
 		Assert.assertThat(mood, CoreMatchers.is("SAD"));
 	}
-
 	@Test
 	public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
 
-		MoodAnalyser moodAnalyzer = new MoodAnalyser();
-		String mood = moodAnalyzer.analyzeMood("This is a happy message");
+		MoodAnalyser moodanalyser = new MoodAnalyser("This is a Happy message");
+		String mood = moodanalyser.analyzeMood();
 		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+	}
+	@Test
+	public void givenMessage_WhenNul_ShouldReturnHapy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser(null);
+		String mood = moodanalyser.analyzeMood();
+		Assert.assertEquals("HAPPY",mood);
 	}
 }
